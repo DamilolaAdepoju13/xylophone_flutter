b305a01c245dfff0a8a6923f688891e96597a9b2
DamilolaAdepoju13 <damilolaadepoju10@gmail.com>
2023-04-04 21:03:39 -0400

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
+void main() => runApp(XylophoneApp());
+
+class XylophoneApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var;
+    var ;
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child:Center(child:TextButton(onPressed:(){
+
+    },child: Text ('Click Me'))),
+        ),
+      ),
+    );
+  }
+}
