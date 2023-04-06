4369b4be7b729a1c793eca4bee7febf9b8fba802
DamilolaAdepoju13 <damilolaadepoju10@gmail.com>
2023-04-05 22:05:55 -0400

    Initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,62 @@
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audio_cache.dart';
+
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+
+  void playSound(int soundNumber){final player = AudioCache();
+  player.play('note$soundNumber.wav');
+
+  }
+
   @override
   Widget build(BuildContext context) {
-    var;
-    var ;
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child:Center(child:TextButton(onPressed:(){
-
-    },child: Text ('Click Me'))),
+          child: Center(
+            child: Column(
+              children: [
+                TextButton(
+                  style: TextButton.styleFrom(backgroundColor: Colors.red),
+                  onPressed: () {
+                   playSound(1);
+                  },
+                ),TextButton(
+                  style: TextButton.styleFrom(backgroundColor: Colors.orange),
+                  onPressed: () {
+                    playSound(2);
+                  },
+                ),TextButton(
+                  style: TextButton.styleFrom(backgroundColor: Colors.yellow),
+                  onPressed: () {
+                    playSound(3);
+                  },
+                ),TextButton(
+                  style: TextButton.styleFrom(backgroundColor: Colors.green),
+                  onPressed: () {
+                    playSound(4);
+                  },
+                ),TextButton(
+                  style: TextButton.styleFrom(backgroundColor: Colors.teal),
+                  onPressed: () {
+                    playSound(5);
+                  },
+                ),TextButton(
+                  style: TextButton.styleFrom(backgroundColor: Colors.blue),
+                  onPressed: () {
+                    playSound(6);
+                  },
+                ),TextButton(
+                  style: TextButton.styleFrom(backgroundColor: Colors.purple),
+                  onPressed: () {
+                    playSound(7);
+                  },
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
